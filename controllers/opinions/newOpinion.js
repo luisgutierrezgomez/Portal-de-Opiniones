e9e750864b339e75323a3ba8bfe4f298b52bcf19
larriety <infaurden@gmail.com>
2022-05-03 15:01:29 +0200

    modificacion archivos opinion

--- controllers/opinions/newOpinion.js
+++ controllers/opinions/newOpinion.js
@@ -1,22 +1,37 @@
+const { getConnection } = require("../../db/db");
 const { generateError } = require("../../helpers");
 
-const newOpinionController = async (req, res, next) => {
-  const { text } = req.body;
-
-  if (!text || text.length > 65535) {
-    throw generateError(
-      "El campo de texto es obligatorio y debe ser como máximo de 65535 caracteres",
-      400
-    );
-  }
+async function newOpinionController(req, res, next) {
+  let connection;
   try {
+    connection = await getConnection();
+
+    const { text } = req.body;
+
+    if (!text || text.length > 65535) {
+      throw generateError(
+        "El campo de texto es obligatorio y debe ser como máximo de 65535 caracteres",
+        400
+      );
+    }
+    const [result] = await connection.query(
+      `
+    INSERT INTO opinions(opinion_id, text, created-at) VALUES (?,?,UTC_TIMESTAMP)
+    `,
+      [req.auth.opinion_id, text]
+    );
     res.send({
-      status: "error",
-      message: "Not implemented",
+      status: "ok",
+      data: {
+        id: result.opinion_id,
+        text,
+      },
     });
   } catch (error) {
     next(error);
+  } finally {
+    if (connection) connection.release();
   }
-};
+}
 
 module.exports = { newOpinionController };
